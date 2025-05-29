4f54cec3b2f108c712d5b269b8c9dbc1a7ae83f1
iliyas <166032805+Ilkasus@users.noreply.github.com>
2025-05-29 17:10:32 +0500

    Update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres@db:5432/metaspace"
+SQLALCHEMY_DATABASE_URL = "postgresql://metaspace_db_user:eN9p7YNSVDXKFlT3n7ElmPDFprKA8Wj4@dpg-d0rctummcj7s7385trg0-a.oregon-postgres.render.com/metaspace_db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
