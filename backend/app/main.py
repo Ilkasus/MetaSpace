251b9634c1fb1ad00245cb2ffe9bf5d2144a143f
iliyas <166032805+Ilkasus@users.noreply.github.com>
2025-06-03 20:00:52 +0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth, users
 from app.db import Base, engine
+
+import socketio
+from socketio import ASGIApp
 import uvicorn
 
-app = FastAPI(title="MetaSpace API")
+fastapi_app = FastAPI(title="MetaSpace API")
+sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
+app = ASGIApp(sio, other_asgi_app=fastapi_app)
 
 Base.metadata.create_all(bind=engine)
 
@@ -16,7 +21,7 @@ origins = [
     "https://meta-space-oo2t7fmki-ilkasus-projects.vercel.app"
 ]
 
-app.add_middleware(
+fastapi_app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
@@ -24,12 +29,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
+@fastapi_app.get("/")
 def root():
     return {"message": "MetaSpace backend is running"}
 
-app.include_router(auth.router, prefix="/auth", tags=["Auth"])
-app.include_router(users.router, prefix="/users", tags=["Users"])
+fastapi_app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+fastapi_app.include_router(users.router, prefix="/users", tags=["Users"])
+
+@sio.event
+async def connect(sid, environ):
+    print(f"🟢 Socket connected: {sid}")
+
+@sio.event
+async def disconnect(sid):
+    print(f"🔴 Socket disconnected: {sid}")
+
+@sio.event
+async def send_message(sid, data):
+    print(f"💬 New message from {sid}: {data}")
+    await sio.emit("receive_message", data)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
