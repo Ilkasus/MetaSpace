9bd71d36c4ab9c18c537b7abbc2a722b893683e2
iliyas <166032805+Ilkasus@users.noreply.github.com>
2025-06-04 19:21:39 +0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ origins = [
     "http://localhost:3000",
     "https://meta-space-nu.vercel.app",
     "https://meta-space-git-main-ilkasus-projects.vercel.app",
-    "https://meta-space-73cpvvgl8-ilkasus-projects.vercel.app"
+    "https://meta-space-74z744fsz-ilkasus-projects.vercel.app"
 ]
 
 fastapi_app = FastAPI(title="MetaSpace API")
